f1d56592d64178a561ede4d2d7c4d5b759a8967d
Ghost-Rider <ghostrider.gu@gmail.com>
2021-06-30 22:08:08 +0300

    Added parallax scrolling app

---
+++ ParallaxScrolling/BackgroundParallax.js
@@ -0,0 +1,41 @@
+﻿import ScrollingBackground from './ScrollingBackground.js';
+
+class BackgroundParallax {
+    constructor(backgroundImages, canvas) {
+        this.backgroundScrollableSprites = [];
+        this.backgroundImages = backgroundImages;
+        this.canvas = canvas;
+        this.canvasContext = canvas.getContext('2d');
+
+        this.init();
+    };
+
+    init() {
+        let speed = 1;
+        for (let i = 0; i < this.backgroundImages.length; i++) {
+            const backgroundImage = new Image();
+            backgroundImage.src = this.backgroundImages[i];
+
+            const backgroudSprite = new ScrollingBackground(backgroundImage, 0, 0, this.canvas.width, this.canvas.height, speed);
+            const backgroundSpriteBack = new ScrollingBackground(backgroundImage, -this.canvas.width, 0, this.canvas.width, this.canvas.height, speed);
+
+            this.backgroundScrollableSprites.push(backgroudSprite);
+            this.backgroundScrollableSprites.push(backgroundSpriteBack);
+
+            speed += 0.5;
+        }
+    }
+
+    render = () => {
+        this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.backgroundScrollableSprites.forEach(background => {
+            background.scrollBackground();
+            background.drawBackground(this.canvasContext);
+        });
+
+        window.requestAnimationFrame(this.render);
+    }
+}
+
+export default BackgroundParallax;
---
+++ ParallaxScrolling/script.js
@@ -0,0 +1,17 @@
+﻿import BackgroundParallax from './BackgroundParallax.js';
+
+function startGame() {
+	const canvas = document.getElementById('parallax');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	const backgroundImages = [];
+	for (let i = 1; i <= 5; i++) {
+		backgroundImages.push(`./images/layer${i}.png`);
+	}
+
+	const backgroundParallax = new BackgroundParallax(backgroundImages, canvas);
+	backgroundParallax.render();
+}
+
+startGame();
