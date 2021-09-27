35543bbbebe5301076204ee74da40a5912b66b0a
Iurii <ghostrider.gu@gmail.com>
2021-09-27 22:19:04 +0300

    init the matrix project

---
+++ TheMatrix/script.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('matrix');
+const context = canvas.getContext('2d');
+
+const TECH_STAFF = [
+    'JavaScript', 'HTML5', 'CSS3', 'Java', 'Delphi',
+    'ReactJS', 'Angular', 'VueJS', 'Spring Boot', 'Spring Security',
+    'Docker', 'Kubernetes', 'Programming', 'DevOps', 'Assembler', 'C++',
+    'C#', 'Python', 'SQL', 'Ruby', 'Go', 'Development'
+];
+
+function showMatrix () {
+  // matrix reload ))
+}
+
+setInterval(showMatrix, 100);
