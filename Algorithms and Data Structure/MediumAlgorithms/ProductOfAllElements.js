1318298bff85461253889e803c56416ac3635d34
Iurii <ghostrider.gu@gmail.com>
2021-11-29 23:00:10 +0300

    added new algo

---
+++ Algorithms and Data Structure/MediumAlgorithms/ProductOfAllElements.js
@@ -0,0 +1,19 @@
+/*
+* Given an array of integers, return a new array such that each element at index i of
+* the new array is the product of all the numbers in the original array except the one at i.
+*
+* For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+* If our input was [3, 2, 1], the expected output would be [2, 3, 6]
+*/
+function getProductOfAllElements(inputArray) {
+    let result = [];
+    var calculated = 1;
+
+    inputArray.forEach(element => { calculated *= element });
+    inputArray.forEach(element => { result.push(calculated / element) });
+
+    return result;
+}
+
+console.log(getProductOfAllElements([1, 2, 3, 4, 5])); // expected result is [120, 60, 40, 30, 24]
+console.log(getProductOfAllElements([3, 2, 1])); // expected result is [2, 3, 6]
