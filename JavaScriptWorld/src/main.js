3d7bac690a09b6a56fb25421b8763fbdd91202aa
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-03-27 19:45:34 +0300

    added additional libraries

--- JavaScriptWorld/src/main.js
+++ JavaScriptWorld/src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap";
 
 const app = createApp(App);
-
-app.use(router);
 
 app.mount("#app");
