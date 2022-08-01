1c4b9c5bf4f27809ad344b831598724291c779b2
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-08-01 21:09:30 +0300

    added example of console.log styling

---
+++ Code Snippets/ConsoleLogStyle.js
@@ -0,0 +1,10 @@
+// Add some styles to you console.log()
+console.log("Hello %cWorld %cIt's me",
+            "color: red;",
+            "background-color: green;");
+
+console.log("Text Eample",
+            `background:linear-gradient(#E66450, #9198E5);
+            padding: 1rem;
+            color: white;
+            border-radius: 0.5em`);
