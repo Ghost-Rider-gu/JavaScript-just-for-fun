248c1f620cd9851622bb93b50eff326eaab10dc7
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-08-11 22:23:05 +0300

    updated router part

--- JavaScriptWorld/src/main.js
+++ JavaScriptWorld/src/main.js
@@ -1,13 +1,17 @@
-import { createApp } from "vue";
-import { createStore } from "vuex";
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
 import { ref } from '@vue/reactivity';
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap";
+import router from './router.js';
+import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap';
+
 import articlesData from './assets/articlesData.json';
 
 const app = createApp(App);
+
 const store = createStore({
     state() {
         return {
@@ -16,5 +20,6 @@ const store = createStore({
     }
 })
 
+app.use(router);
 app.use(store);
 app.mount("#app");
--- JavaScriptWorld/src/router.js
+++ JavaScriptWorld/src/router.js
@@ -1,14 +1,23 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
+import NotFoundComponent from './components/NotFound.vue';
+import StatisticsComponent from './components/Statistics.vue';
+import AboutComponent from './components/About.vue';
+import LoginFormComponent from './components/LoginForm.vue';
+import RegisterFormComponent from './components/RegisterForm.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: null },
         { path: '/search/:searchTerm', component: null },
-        { path: '/statistics', component: null },
         { path: '/refresh', component: null },
-        { path: '/about',  component: null },
-        { path: '/:notFound(.*)',  component: null }
+        { path: '/statistics', component: StatisticsComponent },
+        { path: '/about',  component: AboutComponent },
+        { path: '/register', component: RegisterFormComponent },
+        { path: '/login', component: LoginFormComponent },
+        { path: '/logout', redirect: '/' },
+        { path: '/:notFound(.*)',  component: NotFoundComponent }
     ]
 });
 
