d512876e9bbad936fa865aba26ce89457b2c7166
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-08-25 16:40:55 +0300

    added axios lib

--- JavaScriptWorld/src/main.js
+++ JavaScriptWorld/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import axios from 'axios';
 import router from './router.js';
 import App from './App.vue';
 import store from './store/store.js';
@@ -6,6 +7,8 @@ import store from './store/store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
+
+axios.defaults.baseURL = '';
 
 const app = createApp(App);
 
