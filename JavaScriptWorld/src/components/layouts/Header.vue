867ff590162efb89c52f0874422c482064fd80b4
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2022-11-04 20:16:20 +0200

    updated Header component styles

--- JavaScriptWorld/src/components/layouts/Header.vue
+++ JavaScriptWorld/src/components/layouts/Header.vue
@@ -1,87 +1,48 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              <img
-                src="../../assets/images/logo.png"
-                alt="JavaScript World logo"
-                class="d-inline-block align-text-top img-fluid"
-              />
-            </a>
-            <div class="collapse navbar-collapse" id="navbarText">
-              <span class="navbar-text slogan">
-                Knowledge base of <strong>JavaScript</strong> for you
-              </span>
-            </div>
-          </div>
-        </nav>
+  <nav class="relative container mx-auto">
+    <div class="flex items-center gap-5">
+      <div class="pt-2">
+        <a href="#">
+          <img src="../../assets/images/logo.png" alt="JavaScript World logo" />
+        </a>
+      </div>
+      <div class="pt-2 hidden lg:flex">
+        <span class="slogan">
+          Knowledge base of <strong>JavaScript</strong> for you
+        </span>  
       </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarText"
-              aria-controls="navbarText"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <button class="btn btn-outline-primary me-2" type="button">
-                    Refresh
-                  </button>
-                </li>
-                <li class="nav-item">
-                  <button class="btn btn-outline-primary me-2" type="button">
-                    Statistics
-                  </button>
-                </li>
-                <li class="nav-item">
-                  <button class="btn btn-outline-primary me-2" type="button">
-                    About
-                  </button>
-                </li>
-              </ul>
-              <form class="d-flex">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-primary" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-        </nav>
+    <div class="hidden md:flex justify-between m-3 p-2 items-center bg-slate-100 md:justify-start md:space-x-10">
+      <div class="hidden md:flex items-center space-x-10 justify-start lg:w-0 lg:flex-1">
+        <a href="#" class="text-slate-700 font-semibold hover:text-blue-500">REFRESH</a>
+        <a href="#" class="text-slate-700 font-semibold hover:text-blue-500">
+          <router-link to="/statistics">STATISTICS</router-link>
+        </a>
+        <a href="#" class="text-slate-700 font-semibold hover:text-blue-500">ABOUT</a>
+        <a href="#" class="flex-row-reverse bg-blue-400 text-white font-semibold px-5 py-1 border-indigo-50 rounded-full shadow-xl hover:bg-blue-500">LOG IN</a>
       </div>
-    </div>
-  </div>
+      <div class="hidden md:flex justify-end space-x-10 bg-slate-100 items-center md:flex-1 lg:w-0">
+        <form>
+          <input type="search" placeholder="Search in articles" aria-label="Search" class="bg-white text-slate-500 font-light px-5 py-1 border border-gray-100 rounded-l-full shadow-xl" />
+          <button class="bg-white text-slate-500 font-light px-5 py-1 baseline border border-gray-100 rounded-r-full shadow-xl hover:bg-gray-100" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>    
+  </nav>
 </template>
 
 <style scoped>
 .slogan {
   font-family: "Acme", sans-serif;
-  font-size: 30px;
+  font-size: 26px;
   background: linear-gradient(
     99deg,
     rgba(238, 188, 78, 1) 0%,
     rgba(84, 83, 83, 1) 50%
   );
   background-clip: text;
-  -webkit-text-fill-color: transparent;
+  -webkit-text-fill-color: transparent;  
 }
 </style>
